8d0eb424006ae0cec396add31ca8692f86752cd2
gongbotao <369977927@qq.com>
2021-08-28 09:41:29 +0800

    feat(test): add some test

---
+++ src/main/java/com/gongbotao/test/Staff.java
@@ -0,0 +1,14 @@
+package com.gongbotao.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Staff {
+    private String name;
+    private Integer age;
+}
